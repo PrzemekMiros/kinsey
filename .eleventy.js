545469af703afd2315d786a3f9c6a1c1b130174e
PrzemekMiros <kontakt@futurewebstudio.pl>
2023-01-10 19:49:23 +0100

    Add template

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/assets/css");
+    eleventyConfig.addPassthroughCopy("./src/assets/js");
+    eleventyConfig.addPassthroughCopy("./src/assets/img");
+    eleventyConfig.addPassthroughCopy("./src/assets/fonts");
+  
+    return {
+      dir: {
+        input: "src",
+        output: "public",
+        includes: "includes"
+      },
+    };
+  };
